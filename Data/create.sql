40ec1768d14abd803f91407c14294714ecfb159f
Crakenar <teo.berguerre@iut2.univ-grenoble-alpes.fr>
2019-10-15 14:35:52 +0200

    oui

---
+++ Data/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE article(
+  Identifiant string,
+  Titre TEXT,
+  libelle TEXT,
+  Auteur TEXT,
+  Type string,
+  Genre TEXT,
+  Disponibilite boolean,
+  Quantite int,
+  Prix REAL
+)
